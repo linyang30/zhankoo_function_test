93426b1431ef1cdff6f710d98a119eb60532e3ff
linyang <linyang30@163.com>
2016-11-23 15:27:56 +0800

    add data test case

--- test_case/search_result_sta.py
+++ test_case/search_result_sta.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 from test_case.models.open_page import open, open_links
 import json
+import re
 
 class SearchResult(unittest.TestCase):
 
@@ -85,30 +86,36 @@ class SearchResult(unittest.TestCase):
     def test_txzhanshang(self):
         '''测试天下展商首页数据，并打开详情页'''
         data1 = {
+            'archileLocationCode': 'Txzhanshang',
+            'extArchileLocationCode':"'NewsFlash_Txzhanshang','Cx_Txzhanshang_xwdsr','Cx_Txzhanshang_qys','Cx_Txzhanshang_dks','Cx_Txzhanshang_mtjj','G20_Txzhanshang_Toutiao','G20_Txzhanshang_Depth','G20_Txzhanshang_LastNews','G20_Txzhanshang_Comment','G20_Txzhanshang_Talk','G20_Txzhanshang_SecrectNews','G20_Txzhanshang_Notes','NewsConference_Txzhanshang_Media','NewsConference_Txzhanshang_Sets','NewsConference_Txzhanshang_Work','NewsConference_Txzhanshang_Watch','NewsConference_Txzhanshang_Top','GuoQing_Txzhanshang_House','GuoQing_Txzhanshang_Car','GuoQing_Txzhanshang_Game','GuoQing_Txzhanshang_Floor','GuoQing_Txzhanshang_Forgain','GuoQing_Txzhanshang_Other','GJH_Txzhanshang_FocuNews','GJH_Txzhanshang_FuncChange','GJH_Txzhanshang_Zhaoshang','GJH_Txzhanshang_SecrectNews','GJH_Txzhanshang_Chuzheng','GJH_Txzhanshang_History','GJH_Txzhanshang_Comment','GaoJH_Txzhashang_Focus','GaoJH_Txzhashang_On','GaoJH_Txzhashang_Exhibitor','GaoJH_Txzhashang_Popular','GaoJH_Txzhashang_Guide','GaoJH_Txzhashang_Booth','GaoJH_Txzhashang_Other','GaoJH_Txzhashang_Events'",
             'pageIndex': 1,
             'pageSize': 20,
             'type': 1,
             'menuIndex': 1
         }
         data2 = {
+            'archileLocationCode': 'Txzhanshang_Special',
             'pageIndex': 1,
             'pageSize': 20,
             'type': 2,
             'menuIndex': 2
         }
         data3 = {
+            'archileLocationCode': 'Txzhanshang_Depth',
             'pageIndex': 1,
             'pageSize': 20,
             'type': 2,
             'menuIndex': 3
         }
         data4 = {
+            'archileLocationCode': 'Txzhanshang_Image',
             'pageIndex': 1,
             'pageSize': 20,
             'type': 2,
             'menuIndex': 4
         }
         data5 = {
+            'archileLocationCode': 'Txzhanshang_Character',
             'pageIndex': 1,
             'pageSize': 20,
             'type': 2,
@@ -117,14 +124,20 @@ class SearchResult(unittest.TestCase):
         datas = [data1, data2, data3, data4, data5]
         for data in datas:
             txzhanshang_ids = []
+            txzhanshang_links = []
+            error_links = []
             web_response = requests.post(self.txzhanshang_data_url, data=data)
-            result_json = json.loads(web_response.text)
-            print(result_json['archiles'][0])
-            # targets = result_json['archiles']
+            dr = re.compile(r'<[^>]+>|\\r\\n', re.S)
+            dd = dr.sub('', web_response.text)
+            count = int(json.loads(dd)['totalCount'])
+            assert count > 0
+            res = '{"res":' + json.loads(dd)['archiles'] + "}"
+            for item in json.loads(res)['res']:
+                txzhanshang_ids.append(item['ID'])
+            for id in txzhanshang_ids:
+                txzhanshang_ids.append('http://txzhanshang.zhankoo.com/detail/%s.html' % id)
+            open_links(txzhanshang_links, error_links)
 
-            # for target in targets:
-            #     txzhanshang_ids.append(target['ID'])
-            #     print(txzhanshang_ids)
 
 
 
