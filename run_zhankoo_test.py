2b35abe8ed12922926d13fbca56d58b92e84f9fc
linyang <linyang30@163.com>
2016-12-10 16:58:57 +0800

    add data test case

--- run_zhankoo_test.py
+++ run_zhankoo_test.py
@@ -2,12 +2,37 @@ from HTMLTestRunner import HTMLTestRunner
 import time
 import os
 import unittest
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
 
+#发送邮件
+def send_mail(report):
+    f = open(report, 'rb')
+    mail_body = f.read()
+    f.close()
+
+    msg = MIMEText(mail_body, 'html', 'utf-8')
+    msg['Subject'] = Header('展酷自动化测试报告', 'utf-8')
+
+
+    smtp = smtplib.SMTP()
+    smtp.connect('smtp.163.com')
+    smtp.login('iron_ly@163.com', '1227wdZ!')
+    smtp.sendmail('iron_ly@163.com', 'linyang@zhankoo.com', msg.as_string())
+    smtp.quit()
+
+#获取最新测试报告文件
+def new_report(report_path):
+    report_list = os.listdir(report_path)
+    report_list.sort(key=lambda x : os.path.getmtime(report_path + '\\' + x))
+    file_new = os.path.join(report_path, report_list[-1])
+    return file_new
 
 
 
 if __name__ == '__main__':
-    now = time.strftime('%Y_%m_%d')
+    now = time.strftime('%Y_%m_%d_%H_%M_%S')
     base_dir = os.getcwd()
     base = base_dir.split('\\test_case')[0]
     filename = base + '\\report\\' + now + '_result.html'
@@ -16,4 +41,6 @@ if __name__ == '__main__':
     discover = unittest.defaultTestLoader.discover(base + '\\test_case',pattern='*_sta.py')
     runner.run(discover)
     fp.close()
+    report_file = new_report(base + '\\report')
+    # send_mail(report_file)
 
